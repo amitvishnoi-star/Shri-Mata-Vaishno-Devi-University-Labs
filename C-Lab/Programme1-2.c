cdb35d35ea1f9d8cfc9b28e095ec8b1afc5da16e
Vishnoi Amit <amit.vishnoi.982@gmail.com>
2025-10-27 18:57:14 +0530

    Programme 2 Added

---
+++ C-Lab/Programme1-2.c
@@ -0,0 +1,11 @@
+// Write A C programme To Find The Sum Of Two Numbers.
+#include <stdio.h>
+int main() 
+{
+    int num1,num2,sum;
+    printf("Enter two numbers: "); 
+    scanf("%d %d", &num1, &num2);
+    sum = num1 + num2;
+    printf("Sum of %d and %d is: %d\n", num1, num2, sum);
+    return 0;
+}
