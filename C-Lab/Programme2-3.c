d22295b7f44ab5d5470bfcc590f0c18eb8eab0d3
Vishnoi Amit <amit.vishnoi.982@gmail.com>
2025-10-29 10:15:29 +0530

    Lab Day2 Programmes Added

---
+++ C-Lab/Programme2-3.c
@@ -0,0 +1,29 @@
+// Write A C programme To Compute 5 Subjects Marks Display Percentage , Total Marks And Pass or Fail. 
+// Use if-else.
+#include <stdio.h>
+int main()
+ {   int maths , science, english, hindi, social;
+    float total, percentage;
+    printf("Enter marks of five subjects (out of 100): ");
+    scanf("%d %d %d %d %d", &maths, &science, &english, &hindi, &social);
+    total = maths + science + english + hindi + social;
+    percentage = (total / 500) * 100;
+    printf("Total Marks: %.2f\n", total);
+    printf("Percentage: %.2f%%\n", percentage);
+    if (percentage >= 35){
+        if(maths<35)
+            printf("Result: Fail\n");
+        else if(science<35)
+            printf("Result: Fail\n");
+        else if(english<35)
+            printf("Result: Fail\n");
+        else if(hindi<35)
+            printf("Result: Fail\n");
+        else if(social<35)
+            printf("Result: Fail\n");
+        else
+        printf("Result: Pass\n");
+    }
+    else
+        printf("Result: Fail\n");
+}
